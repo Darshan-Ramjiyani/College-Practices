0e340c22f91866e8b0cc04c7b3a42ea351ce69c4
Darshan Ramjiyani <ahamdsp@protonmail.com>
2022-04-25 14:53:06 +0530

    Fomate files.

--- Java-Web/DatabaseAndSession/src/java/User.java
+++ Java-Web/DatabaseAndSession/src/java/User.java
@@ -1,14 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
  *
- * @author dsp
+ * @author DSP
  */
 
-public class User {
+public class User 
+{
 
     private int id;
     private String fullName, gender, phoneNumber, address, Hobbies, username, password;
